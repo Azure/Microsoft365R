58fcf5bde39e547e859945bb617445b30abfff4c
Hong Ooi <hongooi73@gmail.com>
2021-02-15 12:42:25 +1100

    add business onedrive tests

--- tests/testthat/test02_business_onedrive.R
+++ tests/testthat/test02_business_onedrive.R
@@ -58,3 +58,74 @@ test_that("OneDrive for Business works",
     expect_silent(od$delete_item(newfolder, confirm=FALSE))
 })
 
+
+test_that("Drive item methods work",
+{
+    od <- get_business_onedrive(tenant=tenant, app=app)
+
+    root <- od$get_item("/")
+    expect_is(root, "ms_drive_item")
+
+    tmpname1 <- make_name(10)
+    folder1 <- root$create_folder(tmpname1)
+    expect_is(folder1, "ms_drive_item")
+    expect_true(folder1$is_folder())
+
+    tmpname2 <- make_name(10)
+    folder2 <- folder1$create_folder(tmpname2)
+    expect_is(folder2, "ms_drive_item")
+    expect_true(folder2$is_folder())
+
+    src <- write_file()
+    expect_silent(file1 <- root$upload(src))
+    expect_is(file1, "ms_drive_item")
+    expect_false(file1$is_folder())
+    expect_error(file1$create_folder("bad"))
+
+    file1_0 <- root$get_item(basename(src))
+    expect_is(file1_0, "ms_drive_item")
+    expect_false(file1_0$is_folder())
+    expect_identical(file1_0$properties$name, file1$properties$name)
+
+    dest1 <- tempfile()
+    expect_silent(file1$download(dest1))
+    expect_true(files_identical(src, dest1))
+
+    expect_silent(file2 <- folder1$upload(src))
+    expect_is(file2, "ms_drive_item")
+
+    dest2 <- tempfile()
+    expect_silent(file2$download(dest2))
+    expect_true(files_identical(src, dest2))
+
+    dest3 <- tempfile()
+    expect_silent(file3 <- folder2$upload(src, basename(dest3)))
+    expect_is(file3, "ms_drive_item")
+    expect_identical(file3$properties$name, basename(dest3))
+    expect_silent(file3$download(dest3))
+    expect_true(files_identical(src, dest3))
+
+    file3_1 <- folder1$get_item(file.path(tmpname2, basename(dest3)))
+    expect_is(file3_1, "ms_drive_item")
+    expect_identical(file3_1$properties$name, file3$properties$name)
+
+    lst0 <- root$list_files()
+    expect_is(lst0, "data.frame")
+    lst0_f <- root$list_files(info="name", full_names=TRUE)
+    expect_type(lst0_f, "character")
+    expect_true(all(substr(lst0_f, 1, 1) == "/"))
+
+    lst0_1 <- root$list_files(tmpname1)
+    lst1 <- folder1$list_files()
+    expect_identical(lst0_1, lst1)
+
+    lst1_f <- folder1$list_files(tmpname2, info="name", full_names=TRUE)
+    expect_type(lst1_f, "character")
+    expect_true(all(grepl(paste0("^", tmpname2), lst1_f)))
+
+    expect_silent(file3$delete(confirm=FALSE))
+    expect_silent(folder2$delete(confirm=FALSE))
+    expect_silent(file2$delete(confirm=FALSE))
+    expect_silent(folder1$delete(confirm=FALSE))
+    expect_silent(file1$delete(confirm=FALSE))
+})

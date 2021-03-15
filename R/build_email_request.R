ac99ca9253654611180c6df100be709f1fdba780
Hong Ooi <hongooi73@gmail.com>
2021-03-15 22:55:52 +1100

    fix build_email_recipients

--- R/build_email_request.R
+++ R/build_email_request.R
@@ -144,7 +144,7 @@ build_email_recipients <- function(to, cc, bcc, reply_to)
     make_recipients <- function(addr_list)
     {
         # NA means don't update current value
-        if(is.na(addr_list))
+        if(!is_empty(addr_list) && is.na(addr_list))
             return(NA)
 
         # handle case of a single az_user object
@@ -176,5 +176,5 @@ build_email_recipients <- function(to, cc, bcc, reply_to)
         bccRecipients=make_recipients(bcc),
         replyTo=make_recipients(reply_to)
     )
-    out[sapply(out, function(x) !is.na(x))]
+    out[sapply(out, function(x) is_empty(x) || !is.na(x))]
 }

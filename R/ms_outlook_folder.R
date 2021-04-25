5ec0e19b76a6c4b13012703632c8ffc4872a65c5
Hong Ooi <hongooi73@gmail.com>
2021-04-25 10:41:46 +1000

    add search arg to list_emails

--- R/ms_outlook_folder.R
+++ R/ms_outlook_folder.R
@@ -148,10 +148,12 @@ public=list(
         super$initialize(token, tenant, properties)
     },
 
-    list_emails=function(by="received desc", n=100, pagesize=10)
+    list_emails=function(by="received desc", search=NULL, n=100, pagesize=10)
     {
         order_by <- email_list_order(by)
-        opts <- list(`$orderby`=order_by, `$top`=pagesize)
+        opts <- if(is.null(search))
+            list(`$orderby`=order_by, `$top`=pagesize)
+        else list(`$search`=search, `$top`=pagesize)
         lst <- private$get_paged_list(self$do_operation("messages", options=opts), n=n)
         private$init_list_objects(lst, default_generator=ms_outlook_email, user_id=self$user_id)
     },

3487e681619a0bfb4dec3a0267c306cc4994f308
Robert Ashton <r.ashton@imperial.ac.uk>
2021-08-27 12:13:26 +0100

    Remove roxygen docs for private methods

--- R/add_methods.R
+++ R/add_methods.R
@@ -2,7 +2,7 @@
 
 #' Microsoft 365 object accessor methods
 #'
-#' Methods for the [`AzureGraph::ms_graph`], [`AzureGraph::az_user`], [`AzureGraph::az_group`] and [`AzureGraph::ms_object`] classes.
+#' Methods for the [`AzureGraph::ms_graph`], [`AzureGraph::az_user`] and [`AzureGraph::az_group`] classes.
 #'
 #' @rdname add_methods
 #' @name add_methods
@@ -52,9 +52,6 @@
 #'
 #' ## R6 method for class 'az_user'
 #' list_teams(filter = NULL, n = Inf)
-#'
-#' ## R6 method for class 'ms_object'
-#' make_basic_list(op, filter, n, ...)
 #' ```
 #' @section Arguments:
 #' - `drive_name`,`drive_id`: For `get_drive`, the name or ID of the drive or shared document library. Note that only the `az_group` method  has the `drive_name` argument, as user drives do not have individual names (and most users will only have one drive anyway). For the `az_user` and `az_group` methods, leaving the argument(s) blank will return the default drive/document library.
@@ -62,8 +59,6 @@
 #' - `team_name`,`team_id`: For `az_user$get_team()`, the name and ID of the site. Provide one or the other, but not both. For `ms_graph$get_team`, you must provide the team ID.
 #' - `plan_title`,`plan_id`: For `az_group$get_plan()`, the title and ID of the site. Provide one or the other, but not both.
 #' - `filter, n`: See 'List methods' below.
-#' - `op` A string, the REST operation.
-#' - `...` Arguments passed to lower-level functions, and ultimately to `AzureGraph::call_graph_endpoint`.
 #' @section Details:
 #' `get_sharepoint_site` retrieves a SharePoint site object. The method for the top-level Graph client class requires that you provide either the site URL or ID. The method for the `az_group` class will retrieve the site associated with that group, if applicable.
 #'
@@ -75,12 +70,10 @@
 #'
 #' `get_chat` retrieves a one-on-one, group or meeting chat, by ID. `list_chats` retrieves all chats that the user is part of.
 #'
-#' `make_basic_list` creates a list of Graph objects. A basic utility called by various Microsoft365R class methods. It is exported to work around issues in how R6 handles classes that extend across package boundaries. It should not be called by the user. If `n` is NULL, returns an iterator object, of class `AzureGraph::ms_graph_pager`. Otherwise, returns a list of individual Graph objects.
-#'
 #' Note that Teams, SharePoint and OneDrive for Business require a Microsoft 365 Business license, and are available for organisational tenants only. Similarly, only Microsoft 365 groups can have associated sites/teams/plans/drives, not any other kind of group.
 #'
 #' @section List methods:
-#' All `list_*` methods and `make_basic_list` have `filter` and `n` arguments to limit the number of results. The former should be an [OData expression](https://docs.microsoft.com/en-us/graph/query-parameters#filter-parameter) as a string to filter the result set on. The latter should be a number setting the maximum number of (filtered) results to return. The default values are `filter=NULL` and `n=Inf`. If `n=NULL`, the `ms_graph_pager` iterator object is returned instead to allow manual iteration over the results.
+#' All `list_*` methods have `filter` and `n` arguments to limit the number of results. The former should be an [OData expression](https://docs.microsoft.com/en-us/graph/query-parameters#filter-parameter) as a string to filter the result set on. The latter should be a number setting the maximum number of (filtered) results to return. The default values are `filter=NULL` and `n=Inf`. If `n=NULL`, the `ms_graph_pager` iterator object is returned instead to allow manual iteration over the results.
 #'
 #' Support in the underlying Graph API for OData queries is patchy. Not all endpoints that return lists of objects support filtering, and if they do, they may not allow all of the defined operators. If your filtering expression results in an error, you can carry out the operation without filtering and then filter the results on the client side.
 #' @section Value:
@@ -93,10 +86,8 @@
 #' For `get_team`, an object of class `ms_team`. For `list_teams`, a list of `ms_team` objects.
 #'
 #' For `get_chat`, an object of class `ms_chat`. For `list_chats`, a list of `ms_chat` objects.
-#'
-#' For `make_basic_list` if `n` is NULL, an iterator object of class `AzureGraph::ms_graph_pager`. Otherwise, a list of individual Graph objects.
 #' @seealso
-#' [`ms_site`], [`ms_drive`], [`ms_plan`], [`ms_team`], [`ms_chat`], [`az_user`], [`az_group`], [`az_object`]
+#' [`ms_site`], [`ms_drive`], [`ms_plan`], [`ms_team`], [`ms_chat`], [`az_user`], [`az_group`]
 #' @examples
 #' \dontrun{
 #'

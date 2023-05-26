8c6991f7136cd487b413774aec10850165c54cf0
Hong Ooi <hongooi73@gmail.com>
2023-05-26 23:05:21 +1000

    add immutable id option, document

--- R/ms_outlook_object.R
+++ R/ms_outlook_object.R
@@ -1,11 +1,15 @@
-# use immutable object IDs when talking to Outlook
+# allow use of immutable object IDs when talking to Outlook
 ms_outlook_object <- R6::R6Class("ms_outlook_object", inherit=ms_object,
 
 public=list(
 
     do_operation=function(op="", ...)
     {
-        outlook_headers <- httr::add_headers(Prefer='IdType="ImmutableId"')
-        super$do_operation(op, ..., outlook_headers)
+        if(getOption("microsoft365r_use_outlook_immutable_ids"))
+        {
+            outlook_headers <- httr::add_headers(Prefer='IdType="ImmutableId"')
+            super$do_operation(op, ..., outlook_headers)
+        }
+        else super$do_operation(op, ...)
     }
 ))
